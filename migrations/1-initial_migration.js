beda80c9ad75ee6184fe9d7671c876e12cca5e83
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-07-06 20:20:57 -0400

    o

---
+++ migrations/1-initial_migration.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "test", deps: []
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "initial_migration",
+    "created": "2019-07-07T00:16:27.310Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+    fn: "createTable",
+    params: [
+        "test",
+        {
+
+        },
+        {}
+    ]
+}];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
---
+++ models/test.js
@@ -0,0 +1,22 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('test', {
+    idTest: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true
+    },
+    Test_Name: {
+      type: DataTypes.STRING(45),
+      allowNull: true
+    },
+    Password: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    tableName: 'test'
+  });
+};
