d2571c81375b024652fc4339905b9154722c00b4
Jeremiah McDonald <45184971+XxJMcDoxX@users.noreply.github.com>
2022-10-15 15:02:49 -0500

    a
    a

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import Header from '../components/Header';
-import { withRouter } from 'react-router-dom';
+import  '../screens/About.css';
+
+
+
 
 
 const About = () => (
   <div>
     <Header />
-    <h1>About</h1>
+    <h1>Jeremiah McDonald</h1>;
+    <h7>352-446-8156 / Jeremiah.mcdonalf@gmail.com</h7>;
+    <h3>PROFILE</h3>
+    <p1>A creative, engaging, full-stack developer with a passion for front-end development and UI Design. Specializes in customer satisfaction through teamwork and maintains a commitment to exceeding expectations. Actively pursuing opportunities to provide programming skills, leadership ability, and enthusiasm to a fast-paced environment as a full-stack developer.</p1>
+    <h3>SKILLS</h3>
+    <p1>CSS, JavaScript (JS), Bootstrap, ES6, AngularJS, ReactJS, Redux, Java, MySQL2, Node, and Express</p1>
+    <h3>EDUCATION & CERTIFICATIONS</h3>
+    <p1>BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY WITH AN ADVANCED SOFTWARE DEVELOPER CERTIFICATE</p1>
+    <p2>Woz U Online Certification for webdevelopment (JavaScript)</p2>
+    <h3>WORK HISTORY</h3>
+    <h5>Clymer Farner Barley ans Assoc.</h5>
+    <h5>GIS ANALYST</h5>
+    <h6>JAN2021 - AUG2022</h6>
+    <p3>	Improve productivity through automating processes and managing numerous feature Classes in Several different Databases. </p3>
+    <p3>	Manage and Digitize both Survey and Engineering CAD data to function in Arc GIS Portal Application. </p3>
+    <p3>	Creates many time-saving models through Model Builder and Python Scripting in order to complete daily routines in a more orderly fashion. </p3>
+    <h5>Clymer Farner Barley ans Assoc.</h5>
+    <h5>SURVEY TECHNICIAN / PARTY CHIEF</h5>
+    <h5>May 2017 - DEC 2020</h5>
+    <p3>	Certified Survey technician level 1</p3>
+    <p3>	Lead on numerous survey jobs that resulted in on-time completion with 0 loss.</p3>
+    <p3>	Maintained all equipment used on a daily business for better operation. </p3>
+    <h5>US ARMY</h5>
+    <h5>INTELLIGENCE ANALYST</h5>
+    <h5>June 2010 - 2013</h5>
+    <p3>	Army Accommodation Award, Army Achievement Medal</p3>
+    <p3>	Lead multiple mission engagements while serving in Operation Enduring Freedom, Afghanistan in 2010</p3>
+    <p3>	Solely responsible for building intelligence preparation presentations that went from the ground soldier to the highest-ranking officer.</p3>
+    <p3>	Gave information guidance of the battleground to provide information and aid in order to effectively engage the enemy.</p3>
+  
   </div>
+ 
 );
 
-export default withRouter (About);
 
+
+
+export default About;
