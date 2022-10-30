a44a45220a97e2d6c8931b16a55e2346c68a0dd2
Jeremiah McDonald <45184971+XxJMcDoxX@users.noreply.github.com>
2022-10-30 12:37:50 -0500

    s
    s

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -4,20 +4,31 @@ import React from 'react';
 import Header from '../components/Header';
 import { withRouter } from 'react-router-dom';
 import  PIC  from './Images/profile_pic.jpg';
+import GB from './Images/golf.jpg';
+import '../screens/Home.css'
+import { Parallax } from 'react-parallax';
 
-
+// const ImageStyle = {
+//     borderRadius:'10% 45%',
+//     justifyContent: 'center',
+//     marginLeft:'15%',
+//     display: "flex",
+//     justifyContent: "center",
+// };
 
 const Home = () => {
 
 
     return (
+        <Parallax strength={100} speed={-10} bgImage={GB}>
         <div class >
             <Header />
             <br />
 
             <div class='text'>
                 <h1>Hello! I am Jeremiah McDonald</h1>
-                <div className = "ProPic">
+                <h2>Welcome to my portfolio page.</h2>
+                <div className='ImageStyle'>
                 <img src ={PIC} alt= "profile pic"></img>
                 </div>
                 
@@ -27,7 +38,7 @@ const Home = () => {
 
         </div>
 
-
+        </Parallax>
     );
 
 };
